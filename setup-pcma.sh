cb7fce04281c814c91b03af23a0204d8884659a2
nobel-von-it <maksimdavydenko12@gmail.com>
2025-11-20 12:17:30 +0700

    fix: extend path

--- setup-pcma.sh
+++ setup-pcma.sh
@@ -4,3 +4,24 @@ LOCAL_BIN="$HOME/.local/bin"
 mkdir -p "$LOCAL_BIN"
 
 install -D -m 755 $(realpath "./pcma") "$LOCAL_BIN"
+
+add_export_path() {
+    local shell=$(basename "$SHELL")
+    case "$shell" in
+        bash)
+            echo "export PATH=$HOME/.local/bin:$PATH" >> $HOME/.bashrc
+            ;;
+        zsh)
+            echo "export PATH=$HOME/.local/bin:$PATH" >> $HOME/.bashrc
+            ;;
+        *)
+            echo "Shell is not supported"
+            ;;
+    esac
+}
+
+if command -v pcma &> /dev/null; then
+    echo "Done!"
+else
+
+fi
