556c0b13147035cdf9ea648af907f70bc0f2fbb4
nobel-von-it <maksimdavydenko12@gmail.com>
2025-12-07 00:04:02 +0700

    feat(configs): tmux

--- setup-links.py
+++ setup-links.py
@@ -20,11 +20,12 @@ CONFIG_NAMES = [
     "minvim:nvim",
     "fish",
     "zed",
+    "tmux",
 ]
 
 
 def check_configs_exist(src_path: Path, config_list: list[str]) -> list[str]:
-    not_founded = []
+    not_founded: list[str] = []
     for config in config_list:
         print(f"  -* Checking {config}")
         orig_name = config.split(":")[0]
@@ -61,25 +62,42 @@ def inquirer_questions() -> tuple[Path, Path, list[str]]:
     return (Path(answers["src"]), Path(answers["dest"]), answers["configs"])
 
 
-def link_configs(src_path: Path, dest_path: Path, config_list: list[str]) -> None:
-    for config in config_list:
-        names = config.split(":")
+def link_config(src_path: Path, dest_path: Path, config: str) -> None:
+    names = config.split(":")
 
-        orig_name, link_name = names if len(names) == 2 else (config, config)
+    orig_name, link_name = names if len(names) == 2 else (config, config)
 
-        full_src_path = src_path / orig_name
-        full_dest_path = dest_path / link_name
-        print(f"  -* Checking {full_dest_path}")
-        if full_dest_path.exists():
-            if full_dest_path.is_symlink():
-                print(f"    -* Removing symlink {full_dest_path}")
-                os.unlink(full_dest_path)
-            else:
-                print(f"    -* {full_dest_path} is not a symlink")
-                print("Aborted")
-                sys.exit(1)
-        os.symlink(full_src_path, full_dest_path)
-        print(f"    -* Linked {config}")
+    full_src_path = src_path / orig_name
+    full_dest_path = dest_path / link_name
+    print(f"  -* Checking {full_dest_path}")
+    if full_dest_path.is_symlink():
+        print(f"    -* Removing symlink {full_dest_path}")
+        os.unlink(full_dest_path)
+    elif full_dest_path.exists():
+        print(f"    -* {full_dest_path} is not a symlink")
+        print("Aborted")
+        sys.exit(1)
+    os.symlink(full_src_path, full_dest_path)
+    print(f"    -* Linked {config}")
+
+
+def tmux_config(src_path: Path, dest_path: Path) -> None:
+    link_config(src_path, dest_path, "tmux")
+
+    tpm_path = dest_path / "tmux/plugins/tpm"
+    tpm_url = "https://github.com/tmux-plugins/tpm"
+    if not tpm_path.exists():
+        if os.system(f"git clone {tpm_url} {tpm_path}") != 0:
+            print("Aborted by git clone for tpm")
+            sys.exit(1)
+
+
+def setup_configs(src_path: Path, dest_path: Path, configs: list[str]) -> None:
+    for config in configs:
+        if config == "tmux":
+            tmux_config(src_path, dest_path)
+        else:
+            link_config(src_path, dest_path, config)
 
 
 def main() -> None:
@@ -92,7 +110,7 @@ def main() -> None:
         sys.exit(1)
     print("Configs found")
 
-    link_configs(src_path, dest_path, config_list)
+    setup_configs(src_path, dest_path, config_list)
     print("DONE")
 
 
