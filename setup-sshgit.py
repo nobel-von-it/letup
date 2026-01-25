61515c279f7ee1fbd56d5ef97631f23469f75a7f
nobel-von-it <maksimdavydenko12@gmail.com>
2026-01-25 23:43:18 +0700

    fix(sshgit.py): explicit lists and .pub

--- setup-sshgit.py
+++ setup-sshgit.py
@@ -12,7 +12,7 @@ INQUIRER_SCRIPT_PATH = Path(__file__).parent / "setup-inquirer.py"
 try:
     import inquirer
 except ImportError:
-    _ = subprocess.run([INQUIRER_SCRIPT_PATH], check=True)
+    _ = subprocess.run([sys.executable, str(INQUIRER_SCRIPT_PATH)], check=True)
     import inquirer
 
 
@@ -51,13 +51,14 @@ def inquirer_questions() -> tuple[str, str]:
 
 def main() -> None:
     email, name = inquirer_questions()
-    _ = subprocess.run(f"git config --global user.email {email}".split(" "), check=True)
-    _ = subprocess.run(f"git config --global user.name {name}".split(" "), check=True)
+    _ = subprocess.run(["git", "config", "--global", "user.email", email], check=True)
+    _ = subprocess.run(["git", "config", "--global", "user.name", name], check=True)
 
     if not KEY_PATH.exists():
         KEY_PATH.parent.mkdir(parents=True, exist_ok=True)
         _ = subprocess.run(
-            f'ssh-keygen -t ed25519 -f {KEY_PATH} -C {email} -N ""', check=True
+            ["ssh-keygen", "-t", "ed25519", "-f", str(KEY_PATH), "-C", email, "-N", ""],
+            check=True,
         )
 
     res = subprocess.run(["ssh-agent", "-s"], capture_output=True, text=True)
@@ -66,11 +67,11 @@ def main() -> None:
         if match:
             os.environ[match.group(1)] = match.group(2)
 
-    _ = subprocess.run(["ssh-add", KEY_PATH], check=True)
+    _ = subprocess.run(["ssh-add", str(KEY_PATH)], check=True)
 
-    pub_key = KEY_PATH.read_text()
+    pub_key = KEY_PATH.with_suffix(".pub").read_text()
     if shutil.which("wl-copy") is not None:
-        _ = subprocess.run(["wl-copy", pub_key], check=True)
+        _ = subprocess.run(["wl-copy"], input=pub_key, text=True, check=True)
 
     print(f"Add this key to GitHub: {GITHUB_SSH_URL}")
     print(f"Add this key to Codeberg: {CODEBERG_SSH_URL}")
