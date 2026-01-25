a96ae6594a9a8729d29449d2c802f0dd625b32f8
nobel-von-it <maksimdavydenko12@gmail.com>
2026-01-25 12:12:01 +0700

    impr(yay): cd syntax

--- setup-yay.sh
+++ setup-yay.sh
@@ -6,9 +6,9 @@ YAY_URL="https://aur.archlinux.org/yay.git"
 
 if [ ! -d "$TARGET_DIR" ]; then
     git clone --depth=1 "$YAY_URL" "$TARGET_DIR"
-    cd "$TARGET_DIR" || exit 1
+    cd "$TARGET_DIR"
     makepkg -si
-    cd ..
+    cd -
 else
     echo "yay is already installed"
 fi
