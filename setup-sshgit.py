d9c67fdac0c7f6f450fc6461ada7c2bf10a2f8ec
nobel-von-it <maksimdavydenko12@gmail.com>
2026-01-25 23:13:43 +0700

    fix(sshgit.py): lib check and explicit subprocess

--- setup-sshgit.py
+++ setup-sshgit.py
@@ -2,15 +2,18 @@
 
 import os
 import re
+import shutil
 import subprocess
 import sys
 from pathlib import Path
 
+INQUIRER_SCRIPT_PATH = Path(__file__).parent / "setup-inquirer.py"
+
 try:
     import inquirer
 except ImportError:
-    print("Please install inquirer")
-    sys.exit(1)
+    _ = subprocess.run([INQUIRER_SCRIPT_PATH], check=True)
+    import inquirer
 
 
 DEFAULT_EMAIL = "maksimdavydenko12@gmail.com"
@@ -42,62 +45,27 @@ def inquirer_questions() -> tuple[str, str]:
     return (answers["email"], answers["name"])
 
 
-def ssh_eval():
+def main() -> None:
+    email, name = inquirer_questions()
+    _ = subprocess.run(f"git config --global user.email {email}".split(" "), check=True)
+    _ = subprocess.run(f"git config --global user.name {name}".split(" "), check=True)
+
+    if not KEY_PATH.exists():
+        _ = subprocess.run(
+            f"ssh-keygen -t ed25519 -f '{KEY_PATH}' -C '{email}'", check=True
+        )
+
     res = subprocess.run(["ssh-agent", "-s"], capture_output=True, text=True)
     for line in res.stdout.splitlines():
         match = re.match(r"(\w+)=([^;]+);", line)
         if match:
             os.environ[match.group(1)] = match.group(2)
 
-
-def exec(args: str, msg: str):
-    res = subprocess.run(args.split(" "))
-    if res.returncode != 0:
-        print(msg)
-        sys.exit(1)
-
-
-def check_package(package: str) -> bool:
-    res = subprocess.run(["command", "-v", package])
-    return res.returncode == 0
-
-
-def check_packages() -> list[str]:
-    packages: list[str] = []
-    if not check_package("git"):
-        packages.append("git")
-    if not check_package("ssh"):
-        packages.append("openssh")
-    if not check_package("wl-copy"):
-        packages.append("wl-clipboard")
-    return packages
-
-
-def main() -> None:
-    packages = check_packages()
-    if packages:
-        exec(
-            f"sudo pacman -S --needed {' '.join(packages)}",
-            "failed to install packages",
-        )
-    email, name = inquirer_questions()
-    exec(f"git config --global user.email {email}", "failed to set email")
-    exec(f"git config --global user.name {name}", "failed to set name")
-
-    if not KEY_PATH.exists():
-        exec(
-            f"ssh-keygen -t ed25519 -f '{KEY_PATH}' -C '{email}'",
-            "failed to generate ssh key",
-        )
-
-    ssh_eval()
-    exec(
-        f"ssh-add {KEY_PATH}",
-        "failed to add ssh key",
-    )
+    _ = subprocess.run(["ssh-add", "-l"], check=True)
 
     pub_key = KEY_PATH.read_text()
-    exec(f"wl-copy '{pub_key}'", "failed to copy key to clipboard")
+    if shutil.which("wl-copy") is not None:
+        _ = subprocess.run(["wl-copy", pub_key], check=True)
 
     print(f"Add this key to GitHub: {GITHUB_SSH_URL}")
     print(f"Add this key to Codeberg: {CODEBERG_SSH_URL}")
