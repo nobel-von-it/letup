6c1d03b58eae2ccc0d01b93a1bb86499a3eb6413
nobel-von-it <maksimdavydenko12@gmail.com>
2026-01-25 22:18:22 +0700

    fix(yay.py): hidden deps and real command check

--- setup-yay.py
+++ setup-yay.py
@@ -4,15 +4,16 @@ import subprocess
 import sys
 from pathlib import Path
 
-DEPS = ["fakeroot", "debugedit", "git", "make", "curl", "go"]
+DEPS = ["fakeroot", "debugedit", "git", "make", "curl", "go", "gcc"]
 
 YAY_DIR = Path(__file__).parent.parent / "yay"
 YAY_URL = "https://aur.archlinux.org/yay.git"
 
-if YAY_DIR.exists():
-    print("yay already installed")
+if subprocess.run(["command", "-v", "yay"]).returncode == 0:
+    print("Yay is already installed.")
     sys.exit(0)
 
 _ = subprocess.run(["sudo", "pacman", "-S", "--needed", *DEPS], check=True)
-_ = subprocess.run(["git", "clone", YAY_URL, YAY_DIR], check=True)
+if not YAY_DIR.exists():
+    _ = subprocess.run(["git", "clone", YAY_URL, YAY_DIR], check=True)
 _ = subprocess.run(["makepkg", "-si"], cwd=YAY_DIR, check=True)
