fafad2314d967383f683b68f8a5b0e163119b050
nobel-von-it <maksimdavydenko12@gmail.com>
2026-01-26 00:04:02 +0700

    impr(inquirer.py): same exec for safety

--- setup-inquirer.py
+++ setup-inquirer.py
@@ -3,11 +3,12 @@
 
 import shutil
 import subprocess
+import sys
 from pathlib import Path
 
 YAY_SCRIPT_PATH = Path(__file__).parent / "setup-yay.py"
 
 if shutil.which("yay") is None and YAY_SCRIPT_PATH.exists():
-    _ = subprocess.run([YAY_SCRIPT_PATH], check=True)
+    _ = subprocess.run([sys.executable, str(YAY_SCRIPT_PATH)], check=True)
 
 _ = subprocess.run(["yay", "-S", "--needed", "python-inquirer"], check=True)
