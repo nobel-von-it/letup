0f0529d64f22d45f0c29c0f341c7e116a0c3f10b
nobel-von-it <maksimdavydenko12@gmail.com>
2026-01-25 21:56:04 +0700

    feat(yay): rewrite script to python

---
+++ setup-yay.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+from pathlib import Path
+
+YAY_DIR = Path(__file__).parent / "yay"
+YAY_URL = "https://aur.archlinux.org/yay.git"
+
+if YAY_DIR.exists():
+    print("yay already installed")
+    sys.exit(0)
+
+_ = subprocess.run(["git", "clone", YAY_URL, YAY_DIR], check=True)
+_ = subprocess.run(["makepkg", "-si"], cwd=YAY_DIR, check=True)
